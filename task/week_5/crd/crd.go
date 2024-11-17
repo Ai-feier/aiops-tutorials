3043c810bc7c8b9108b0ff8199e2447f0d6d6e47
Ai-feier <2664586998@qq.com>
2024-11-17 21:42:48 +0800

    update

--- task/week_5/crd/crd.go
+++ task/week_5/crd/crd.go
@@ -1,1 +1,87 @@
-package crd
+package main
+
+import (
+	"context"
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	var err error
+	var config *rest.Config
+
+	var kubeconfig *string
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	pd := filepath.Join(pwd, "config")
+	kubeconfig = &pd
+
+	// 初始化 rest.Config 对象
+	if config, err = rest.InClusterConfig(); err != nil {
+		if config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig); err != nil {
+			panic(err.Error())
+		}
+	}
+
+	config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// 获取客户端和映射器
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	discoveryClient := clientset.Discovery()
+	apiGroupResources, err := restmapper.GetAPIGroupResources(discoveryClient)
+	if err != nil {
+		panic(err)
+	}
+
+	mapper := restmapper.NewDiscoveryRESTMapper(apiGroupResources)
+
+	// 动态映射 Kind 到 GVR
+	// gvk := schema.FromAPIVersionAndKind("mygroup.example.com/v1alpha1", kind)
+	// 还可以用这个方法
+	gvk := schema.GroupVersionKind{
+		Group:   "aiops.geektime.com",
+		Version: "v1alpha1",
+		Kind:    "AIOps",
+	}
+
+	mapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		panic(err)
+	}
+	// mapping.Resource 就是 GVR，这样就实现 GVK->GVR 的转化
+
+	// 获取资源
+	resourceInterface := dynamicClient.Resource(mapping.Resource).Namespace("default")
+	resources, err := resourceInterface.List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	// 打印资源
+	for _, resource := range resources.Items {
+		fmt.Printf("Name: %s, Namespace: %s, UID: %s\n", resource.GetName(), resource.GetNamespace(), resource.GetUID())
+	}
+}
